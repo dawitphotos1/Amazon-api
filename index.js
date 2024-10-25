3db502f1bf5c05284d58ce754bd48def3346c387
Dawit <dawitphotos1@gmail.com>
2024-10-24 20:11:38 -0400

    updtate

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 
-
 dotenv.config();
 const stripe = require("stripe")(process.env.STRIPE_KEY);
 
@@ -16,27 +15,25 @@ app.get("/", (req, res) => {
   });
 });
 
-app.post("/payment/create",async(req,res)=>{
-  const total=req.query.total;
-  if (total>0){
-    
-    const paymentIntent=await stripe.paymentIntents.create({
-      amount:total,
-      currency:"usd"
-    })
-     console.log(paymentIntent);
-     res.status(201).json({
-       clientSecret: paymentIntent.client_secret,
-     });
-  }else {
+app.post("/payment/create", async (req, res) => {
+  const total = parseInt(req.query.total);
+  if (total > 0) {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: total,
+      currency: "usd",
+    });
+    console.log(paymentIntent);
+    res.status(201).json({
+      clientSecret: paymentIntent.client_secret,
+    });
+  } else {
     res.status(403).json({
-      message:"total must be greater than 0",
+      message: "total must be greater than 0",
     });
   }
-})
+});
 
 app.listen(5000, (err) => {
   if (err) throw err;
-  console.log("Amazon Server Running on PORT:5000,http://localhost:5000")
+  console.log("Amazon Server Running on PORT:5000,http://localhost:5000");
 });
-
